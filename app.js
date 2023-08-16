bc6649353080663f767228d8f00f047ca2192064
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-08-16 18:15:20 -0400

    Add functionality to book buttons and consolidated functions

--- app.js
+++ app.js
@@ -1,107 +1,123 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    function Book(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-        this.info = () => {
-            return read
-                ? `${this.title} by ${this.author}, ${this.pages} pages, read already`
-                : `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
-        };
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () => {
+        return read
+            ? `${this.title} by ${this.author}, ${this.pages} pages, read already`
+            : `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
     };
+    this.toggleReadStatus = () => {
+        this.read = !this.read;
+    };
+}
 
-    function addBookToLibrary(title, author, pages, read) {
-        myLibrary.push(new Book(title, author, pages, read));
-    }
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
+}
 
 
-    function updateLibraryDisplay() {
-        container.innerHTML = ""; // Clear the current display
+function clearInputs() {
+    [titleInput.value, authorInput.value, pagesInput.value, readInput.value] = [
+        "",
+        "",
+        "",
+        "",
+    ];
+}
 
-        myLibrary.forEach((book) => {
-            const card = document.createElement("div");
-            card.classList.add("card");
+function buildLibrary() {
+    container.innerHTML = ""; // Clear the current display for updating the library
 
-            const titleSpot = document.createElement("h1");
-            titleSpot.textContent = book.title;
-            card.appendChild(titleSpot);
-
-            const authorSpot = document.createElement("h2");
-            authorSpot.textContent = book.author;
-            card.appendChild(authorSpot);
-
-            const pagesSpot = document.createElement("p");
-            pagesSpot.textContent = book.pages;
-            card.appendChild(pagesSpot);
-
-            const readSpot = document.createElement("p");
-            readSpot.textContent = book.read;
-            card.appendChild(readSpot);
-
-            container.appendChild(card);
-        });
-    }
-
-    const myLibrary = [];
-
-    addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-    addBookToLibrary("Awaken the Giant Within", "Anthony Robbins", 538, true)
-    addBookToLibrary("Dune", "Frank Herbert", 884, true)
-
-    const container = document.querySelector(".grid-container");
-
-    myLibrary.forEach(book => {
+    myLibrary.forEach((book, index) => {
         const card = document.createElement("div");
         card.classList.add("card");
 
         const titleSpot = document.createElement("h1");
+        titleSpot.classList.add("content");
         titleSpot.textContent = book.title;
         card.appendChild(titleSpot);
 
         const authorSpot = document.createElement("h2");
+        authorSpot.classList.add("content");
         authorSpot.textContent = book.author;
         card.appendChild(authorSpot);
 
         const pagesSpot = document.createElement("p");
+        pagesSpot.classList.add("content");
         pagesSpot.textContent = book.pages;
         card.appendChild(pagesSpot);
 
-        const readSpot = document.createElement("p");
-        readSpot.textContent = book.read;
+        const readSpot = document.createElement("button");
+        readSpot.classList.add("content");
+        const readText = document.createElement("p")
+        readText.textContent = book.read ? "Read" : "Not Read";
+        readSpot.appendChild(readText);
+        readSpot.classList.add("read-status", "book-button");
+        readSpot.addEventListener("click", () => {
+            book.toggleReadStatus();
+            buildLibrary();
+        });
         card.appendChild(readSpot);
+
+        const removeButton = document.createElement("button");
+        const removeText = document.createElement("p");
+        removeText.textContent = "Remove";
+        removeButton.appendChild(removeText);
+        removeButton.classList.add("book-button");
+        removeButton.addEventListener("click", () => {
+            myLibrary.splice(index, 1);
+            buildLibrary();
+        })
+        card.appendChild(removeButton);
 
         container.appendChild(card);
     });
+};
 
-    const showButton = document.getElementById("showDialog");
-    const addBookDialog = document.getElementById("addBookDialog");
-    const confirmButton = addBookDialog.querySelector("#confirmBtn");
+const myLibrary = [];
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("Awaken the Giant Within", "Anthony Robbins", 538, true);
+addBookToLibrary("Dune", "Frank Herbert", 884, true);
 
-    showButton.addEventListener("click", (event) => {
-        addBookDialog.showModal();
-    });
+const container = document.querySelector(".grid-container");
+const showButton = document.getElementById("showDialog");
+const addBookDialog = document.getElementById("addBookDialog");
+const confirmButton = addBookDialog.querySelector("#confirmBtn");
+const cancelButton = addBookDialog.querySelector("#cancelBtn");
 
-    confirmButton.addEventListener("click", (event) => {
-        event.preventDefault();
+const titleInput = document.querySelector(".title-input");
+const authorInput = document.querySelector(".author-input");
+const pagesInput = document.querySelector(".pages-input");
+const readInput = document.querySelector(".read-input");
 
-        const titleInput = document.querySelector(".title-input");
-        const authorInput = document.querySelector(".author-input");
-        const pagesInput = document.querySelector(".pages-input");
-        const readInput = document.querySelector(".read-input");
-
-        const readValue = readInput.value.toLowerCase() === "yes" ? true : false;
-    
-        addBookToLibrary(
-            titleInput.value,
-            authorInput.value,
-            pagesInput.value,
-            readValue
-        );
-
-        updateLibraryDisplay();
-        addBookDialog.close();
-    });
-
+showButton.addEventListener("click", () => {
+    addBookDialog.showModal();
 });
+
+cancelButton.addEventListener("click", () => {
+    clearInputs();
+    addBookDialog.close();
+});
+
+confirmButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const readValue = readInput.value.toLowerCase() === "yes" ? true : false;
+
+    addBookToLibrary(
+        titleInput.value,
+        authorInput.value,
+        pagesInput.value,
+        readValue
+    );
+    clearInputs();
+    buildLibrary();
+    addBookDialog.close();
+});
+
+buildLibrary();
+
+//
+// Cancel Button
